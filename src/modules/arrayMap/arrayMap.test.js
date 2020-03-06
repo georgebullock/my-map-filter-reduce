71dc48b415a57cf8d5c57fdd144f9a1567405dc2
George <george.bullock@gmx.de>
2020-03-06 20:31:05 +0100

    Remove unnecessary describe blocks

--- src/modules/arrayMap/arrayMap.test.js
+++ src/modules/arrayMap/arrayMap.test.js
@@ -1,14 +1,6 @@
 // @flow
 import { arrayMap } from './arrayMap';
 
-describe('describe arrayMap', () => {
-	test('it should return [2, 4, 6, 8, 10]', () => {
-		expect(arrayMap([1, 2, 3, 4, 5], item => item * 2)).toEqual([
-			2,
-			4,
-			6,
-			8,
-			10
-		]);
-	});
+test('it should return [2, 4, 6, 8, 10]', () => {
+	expect(arrayMap([1, 2, 3, 4, 5], item => item * 2)).toEqual([2, 4, 6, 8, 10]);
 });
