6cb48e83a5bbd8e7e7c7ca6fcd60fe24b505d8a5
George <george.bullock@gmx.de>
2020-03-06 20:14:38 +0100

    Add filter implementation

---
+++ src/modules/arrayFilter/arrayFilter.test.js
@@ -0,0 +1,12 @@
+// @flow
+import { arrayFilter } from './../arrayFilter/arrayFilter';
+
+describe('Describe array map', () => {
+	test('It should return [0, 2, 4, 6, 8 10]', () => {
+		expect(
+			arrayFilter([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], item => {
+				return item % 2 === 0;
+			})
+		).toEqual([0, 2, 4, 6, 8, 10]);
+	});
+});
