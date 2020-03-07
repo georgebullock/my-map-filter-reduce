0462361fa28a86dcc81073de0e7f4478ecc2eef3
George <george.bullock@gmx.de>
2020-03-07 16:59:07 +0100

    MMFR-003: fix arrayReduce. Add two tests.

--- src/modules/arrayReduce/arrayReduce.js
+++ src/modules/arrayReduce/arrayReduce.js
@@ -1,3 +1,15 @@
-export function arrayReduce() {
-	return true;
+// @flow
+
+export function arrayReduce(
+	array: mixed[],
+	callback: function,
+	initialValue: any
+) {
+	let value: any = initialValue ? initialValue : array[0];
+
+	for (let i = initialValue ? 0 : 1; i < array.length; i++) {
+		value = callback(value, array[i], i, array);
+	}
+
+	return value;
 }
--- src/modules/arrayReduce/arrayReduce.test.js
+++ src/modules/arrayReduce/arrayReduce.test.js
@@ -1,5 +1,29 @@
 import { arrayReduce } from './arrayReduce';
 
-test('It should return true', () => {
-	expect(arrayReduce()).toBe(true);
+const numbers: mixed[] = [0, 1, 2, 3, 4, 5];
+
+describe('Describe arrayReduce', () => {
+	// No ititial value
+	test('It should return 15', () => {
+		expect(
+			arrayReduce(numbers, (acc, curr) => {
+				acc += curr;
+				return acc;
+			})
+		).toBe(15);
+	});
+
+	// With ititial value
+	test('It should return 30', () => {
+		expect(
+			arrayReduce(
+				numbers,
+				(acc, curr) => {
+					acc += curr;
+					return acc;
+				},
+				10
+			)
+		).toBe(25);
+	});
 });
