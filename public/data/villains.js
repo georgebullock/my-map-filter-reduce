f94d88d0e2beedd98faa45146c2ebaa33ae0df28
George <george.bullock@gmx.de>
2020-03-07 20:44:05 +0100

    MMFR-004: Add more advanced arrayReduce test

---
+++ public/data/villains.js
@@ -0,0 +1,83 @@
+export const villains = [
+	{
+		name: 'Hugo Drax',
+		bio: "Plotted to poison Earth's atmosphere from space via a nerve agent.",
+		picture: 'https://dummyimage.com/600x400/000000/fff&text=D',
+		skills: [
+			{
+				html: 50
+			},
+			{
+				css: 30
+			},
+			{
+				javaScript: 95
+			}
+		]
+	},
+	{
+		name: 'Alec Trevelyan',
+		bio: "Mastermined a plot to use an EMP weapon to destroy London's economy",
+		picture: 'https://dummyimage.com/600x400/000000/fff&text=T',
+		skills: [
+			{
+				html: 50
+			},
+			{
+				css: 30
+			},
+			{
+				javaScript: 95
+			}
+		]
+	},
+	{
+		name: 'Auric Goldfinger',
+		bio: 'Planned to irradiate the gold in Fort Knox with a nuclear weapon.',
+		picture: 'https://dummyimage.com/600x400/000000/fff&text=G',
+		skills: [
+			{
+				html: 50
+			},
+			{
+				css: 30
+			},
+			{
+				javaScript: 95
+			}
+		]
+	},
+	{
+		name: 'Ernst Stavro Blofeld',
+		bio:
+			'Sought to steal nuclear bombs and use them to extort money from world governments.',
+		picture: 'https://dummyimage.com/600x400/000000/fff&text=B',
+		skills: [
+			{
+				html: 50
+			},
+			{
+				css: 30
+			},
+			{
+				javaScript: 95
+			}
+		]
+	},
+	{
+		name: 'Max Zorin',
+		bio: 'Wanted to start a microchip monopoly by destroying Silicon Valley.',
+		picture: 'https://dummyimage.com/600x400/000000/fff&text=Z',
+		skills: [
+			{
+				html: 50
+			},
+			{
+				css: 30
+			},
+			{
+				javaScript: 95
+			}
+		]
+	}
+];
